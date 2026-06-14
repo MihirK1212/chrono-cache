7b3d58f391e11ed0e3e8159681fd447cdb451123
MihirK1212 <mihirkkarandikar@gmail.com>
2026-06-14 15:28:55 +0000

    adasd

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,15 +1,23 @@
 #include <string>
+#include <cstring>
 
 #include "chrono_cache.h"
 #include "server.h"
 
 
-int main() {
+int main(int argc, char* argv[]) 
+{
+    bool disable_kafka = false;
+    for (int i = 1; i < argc; i++) {
+        if (std::strcmp(argv[i], "--no-kafka") == 0) {
+            disable_kafka = true;
+        }
+    }
 
     CacheConfig config(
         "localhost:9092",
         "chrono-events",
-        false
+        disable_kafka
     );
     ChronoCache cache(config);
 
